d5bcd052fc7eeb0637ffaa636b2da68d74782c76
collins ihezie <collinsihezie6@gmail.com>
2022-09-21 08:01:58 +0100

    added typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { UserService } from './users/users.service';
+import { User } from './users/model/user.entity';
+import { Report } from './reports/model/report.entity';
+
 
 @Module({
-  imports: [UsersModule, ReportsModule],
+  imports: [TypeOrmModule.forRoot({
+    type: 'sqlite',
+    database: 'db.sqlite',
+    entities: [User, Report],
+    synchronize: true
+  }), UsersModule, ReportsModule],
   controllers: [AppController],
   providers: [AppService, UserService],
 })
+
 export class AppModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './model/user.entity';
 import { UsersController } from './users.controller';
 
 @Module({
+  //creates the repository
+  imports: [TypeOrmModule.forFeature([User])],
+  
   controllers: [UsersController]
 })
 export class UsersModule {}
